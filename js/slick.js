5ffe1f48fac815781e5888da92b1fc4bc35e034e
thoainhu <ntthoainhu@gmail.com>
2023-04-24 18:00:58 +0700

    change title

--- js/slick.js
+++ js/slick.js
@@ -1,42 +1,111 @@
-$(".typebooking").slick({
-  slidesToShow: 8,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  dots: false,
-  arrows: false,
-})
-$(".picture").slick({
-  slidesToShow: 4,
-  slidesToScroll: 2,
-  autoplay: true,
-  autoplaySpeed: 5000,
-  dots: false,
-})
-$(".popular-slide").slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  dots: false,
-  centerMode: true,
-  focusOnSelect: true,
-  arrows: false,
-  autoplay: true,
-  autoplaySpeed: 3000,
-})
+$(function () {
+  $(".typebooking").slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+    ],
+  })
+  $(".picture").slick({
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  })
+  $(".popular-slide").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          centerMode: false,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          centerMode: false,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+        },
+      },
+    ],
+  })
 
-$(".feedback-list").slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  dots: false,
-  arrows: false,
-})
-$(".gallery").slick({
-  infinite: true,
-  speed: 2000,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  autoplay: true,
-  centerMode: true,
+  $(".feedback-list").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    arrows: false,
+  })
+  $(".gallery").slick({
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    centerMode: true,
+  })
 })
